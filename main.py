ba82616a1d7026e61d46a6382f33abf7163387d7
Kassicus <kason.suchow@gmail.com>
2021-09-09 13:23:19 -0600

    Finished Average system
    Value can be incremented with the yellow up and down arrows. The green and purple icons are save and load, however the load icon will probably not be used as the load function is called on startup.

--- main.py
+++ main.py
@@ -21,6 +21,35 @@ height = 600
 title = "Helpdesk v3"
 
 percentFill = (150, 140, 235)
+averageFill = (251, 244, 106)
+
+####################################
+#          BUTTON CLASS            #
+####################################
+
+class Button():
+    def __init__(self, x, y, width, height, image, function):
+        self.x = x
+        self.y = y
+
+        self.width = width
+        self.height = height
+
+        self.image = image
+
+        self.function = function
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+    def update(self, events):
+        pos = pygame.mouse.get_pos()
+
+        if self.x <= pos[0] <= self.x + self.width:
+            if self.y <= pos[1] <= self.y + self.height:
+                for event in events:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        self.function()
 
 ####################################
 #          SLIDER CLASS            #
@@ -54,11 +83,50 @@ class Average():
 
         self.image = pygame.image.load("assets/ui/sliders/average.png")
 
+        self.font = pygame.font.Font("assets/fonts/VT323/VT323-Regular.ttf", 40)
+
         self.value = 60
         self.offset = int(self.value * 5)
 
+        self.valueText = self.font.render(str(self.value), False, averageFill)
+
+        self.incButton = Button(400, 110, 50, 15, pygame.image.load("assets/ui/buttons/averageIncButton.png"), self.incValue)
+        self.decButton = Button(400, 180, 50, 15, pygame.image.load("assets/ui/buttons/averageDecButton.png"), self.decValue)
+
+        self.saveButton = Button(475, 110, 50, 35, pygame.image.load("assets/ui/buttons/averageSaveButton.png"), self.saveValue)
+        self.loadButton = Button(475, 160, 50, 35, pygame.image.load("assets/ui/buttons/averageLoadButton.png"), self.loadValue)
+
     def draw(self, surface):
         surface.blit(self.image, (self.x + self.offset, self.y))
+        surface.blit(self.valueText, (408, 132))
+
+        self.incButton.draw(surface)
+        self.decButton.draw(surface)
+        self.saveButton.draw(surface)
+        self.loadButton.draw(surface)
+
+    def update(self, events):
+        self.offset = int(self.value * 5)
+        self.valueText = self.font.render(str(self.value), True, averageFill)
+
+        self.incButton.update(events)
+        self.decButton.update(events)
+        self.saveButton.update(events)
+        self.loadButton.update(events)
+
+    def incValue(self):
+        if self.value < 100:
+            self.value += 1
+
+    def decValue(self):
+        if self.value > 1:
+            self.value -= 1
+
+    def saveValue(self):
+        pickle.dump(self.value, open("data/average/value.p", "wb"))
+
+    def loadValue(self):
+        self.value = pickle.load(open("data/average/value.p", "rb"))
 
 ####################################
 #            MAIN CLASS            #
@@ -79,6 +147,8 @@ class Window():
         self.average = Average()
 
     def start(self):
+        self.average.loadValue()
+
         while self.running:
             self.events = pygame.event.get()
 
@@ -97,6 +167,8 @@ class Window():
         self.average.draw(self.screen)
 
     def update(self):
+        self.average.update(self.events)
+
         pygame.display.update()
         self.clock.tick(30)
 
